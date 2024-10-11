ac168c12961c24fe3b3278f36f0d89d0ccea1c37
takerumiyazawa <t02772953@gmail.com>
2024-10-11 17:29:16 +0900

    ver0.2

---
+++ correct_script.js
@@ -0,0 +1,24 @@
+// 特定のセルの座標
+const specialCells = [
+    [0, 1], 
+    [2, 3], 
+    [0, 4], 
+    [4, 3], 
+    [3, 0]  
+];
+
+// ページ読み込み時に特定のセルから文字を抜き出して表示
+window.onload = function () {
+    let extractedString = '';
+
+    // 特定のセルから文字を抽出
+    for (let i = 0; i < specialCells.length; i++) {
+        const row = specialCells[i][0];
+        const col = specialCells[i][1];
+        const cellValue = document.getElementById(`cell-${row}-${col}`).value;
+        extractedString += cellValue;
+    }
+
+    // 抜き出した文字列を表示
+    document.getElementById('extracted-letters').innerText = '特徴: ' + extractedString;
+};
